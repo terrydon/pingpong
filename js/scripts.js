ee9296a3b51b8e20504cdd1f2a2da2996de94d3b
terrydon <twamboga@gmail.com>
2018-07-02 10:34:31 +0300

    arranging my code

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,54 @@
 // backend logic
-var i = "ping";
-var o = "pong";
-var pp = "pingpong";
-var output;
-var func = function stuff(){
-  for(var number = 1; number <= output; number++){
-        if(number % 15 === 0) {
-        return pp;
-      }else if(i % 5 === 0) {
-      return o;
-      }else if(i % 3 === 0) {
-        return i;
+
+var func = function (stuff){
+
+  var i = "ping";
+  var o = "pong";
+  var pp = "pingpong";
+  var result = [];
+
+
+  for(var number = 1; number <= stuff; number++){ //using for loop
+      if(number % 15 === 0) { // using elseif statement
+        result.push(pp);
+      }else if(number % 5 === 0) {
+      result.push(o);
+    }else if(number % 3 === 0) {
+        result.push(i);
       }else{
-        return number;
+        result.push(number);
       }
+
   }
+  return result;
  };
-
- // looping for
-
-
-
-
-
-
-
-
 
 
 //frontend logic
-$(document).ready(function(){
- $("#submitform").submit(function(event){
+/*$(document).ready(function() {
+ $("form#submitform").submit(function(event){
    event.preventDefault();
+   var user = $("#text-Input").val();
 
-   var output = $("#text-Input").val();
-   var display = func(output);
+   var numbers = func(user);
+   numbers.forEach(function (number) {
+      $("#output").append('<li>' + number + '</li>');
+   })
 
-
-     //$("#output").append('<li>' + item + '</li>');
 
 
  });
-});
+});*/
+$(document).ready(function () {
+  $("form#submitform").submit(function (event) {
+    event.preventDefault();
+
+    var user = $("#text-Input").val();
+    var numbers = func(user);
+
+    numbers.forEach(function (number) {
+       $("#output").append('<li>' + number + '</li>');
+    })
+
+  })
+})
